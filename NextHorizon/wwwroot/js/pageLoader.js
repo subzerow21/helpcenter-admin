86c97819d3aa5091ce11c7e8ed23b76f3c1d6ca4
gxluhh <angelamats340@gmail.com>
2026-03-12 13:45:48 +0800

    Separated Page Loader CSS and JS

---
+++ NextHorizon/wwwroot/js/pageLoader.js
@@ -0,0 +1,9 @@
+﻿// Use logic to show loader during transitions
+window.addEventListener("beforeunload", function () {
+    document.getElementById("loading-overlay").style.display = "flex";
+});
+
+// Hide loader once the new page has fully loaded
+window.addEventListener("load", function () {
+    document.getElementById("loading-overlay").style.display = "none";
+});
